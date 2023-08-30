a0a6733ba057704046cb6c7942328d20012d03ea
D4v1dHu0 <88366550+D4v1dHu0@users.noreply.github.com>
2023-08-30 20:29:55 +1000

    Code redesign
    After thinking about the problem, realised that I don't need to allocated advisor or markers. Readjusted the info i have, need and the form of presentation outputted.

--- presentation_scheduler/PresentationScheduler.js
+++ presentation_scheduler/PresentationScheduler.js
@@ -1,130 +1,66 @@
-// Assumed Data Structure
+/*Data we need to fetch:
+- StudentID
+- AdvisorID
+- Marker1ID
+- Marker2ID (if exists)
+- Project Title
+*/
 
-// const students = [
-//     {
-//         id: '46012345', //should keep as string or change to int?
-//         name: 'John Smith',
-//         discipline: 'Software Engineering',
-//         thesisTitle: 'The art of fucking around and finding out',
-//         advisorId: '40123456',
-//         markerId: '40123456',
-//         needsSecondMarker: true //if need a second marker
-//     },
-//     // ... other student objects ...
-// ];
+/*Data we need to feed the alg:
+- List of Class numbers e.g. ["4RPD101"."4RPD102","4RPD103"]
+- Dates set for presentation (2-3 dates in the form of DD/MM/YYYY), 
+- Advisors’ date preference (Same form as dates set for presentation)
+*/
 
-// const advisors = [
-//     {
-//         id: 'advisor1',
-//         name: 'Advisor 1',
-//         discipline: 'Computer Science',
-//         sessionPreference: ['am', 'pm'], // Example: Both morning and afternoon
-//         assignedStudents: ['student1', 'student2'], // IDs of assigned students
-//     },
-//     // ... other advisor objects ...
-// ];
 
-// const markers = [
-//     {
-//         id: 'marker1',
-//         name: 'Marker 1',
-//         discipline: 'Computer Science',
-//         dayPreference: ['Monday', 'Tuesday'], // Example: Available on Monday and Tuesday
-//         assignedStudents: ['student1', 'student3'], // IDs of assigned students
-//     },
-//     // ... other marker objects ...
-// ];
-
-//Fetching Data
+//TODO: NEED TO FETCH DATA SOMEHOW
+//PROBLEM: Still need a way to simulate fetch data in order to test code!!!
 async function fetchData(url) {
     const response = await fetch(url);
     const data = await response.json();
     return data;
 }
 
-//Define session timing
-const AM_START_HOUR = 9;
-const PM_START_HOUR = 12;
-const NUM_TIME_SLOTS_PER_SESSION = 12;
+// Assuming we have fetched the required data
 
-//to be populated with time slots
-const timeSlots = [];
+function generatePresentationSchedule(studentData, advisorData, classroomNumbers, presentationDates) {
+    const schedule = [];
 
-// Generate time slots for AM session "hour:minute am"
-for (let i = 0; i < NUM_TIME_SLOTS_PER_SESSION; i++) {
-    const hour = AM_START_HOUR + Math.floor(i / 4);
-    const minute = (i % 4) * 15;
-    timeSlots.push(`${hour}:${minute === 0 ? '00' : minute} am`);
-}
+    for (const advisor of advisorData) {
+        const advisorStudents = studentData.filter(student => student.advisorId === advisor.id);
 
-// Generate time slots for PM session "hour:minute pm"
-for (let i = 0; i < NUM_TIME_SLOTS_PER_SESSION; i++) {
-    const hour = PM_START_HOUR + Math.floor(i / 4);
-    const minute = (i % 4) * 15;
-    timeSlots.push(`${hour}:${minute === 0 ? '00' : minute} pm`);
-}
+        // Sort advisorStudents based on project preferences if needed
 
-async function main() {
-    const studentData = await fetchData('API_URL/students'); //replace URLs with actual ones
-    const advisorData = await fetchData('API_URL/advisors');
-    const markerData = await fetchData('API_URL/markers');
-    const schedule = generateSchedule(studentData, advisorData, markerData);
-    applyManualOverrides(schedule, studentData);
-
-    // Output the generated schedule
-    console.log(schedule);
-}
-
-function generateSchedule(studentData, advisorData, markerData) {
-    const schedule = {}; // Mapping of student IDs to assigned time slots
-    
-    for (const student of studentData) {
-        const advisor = advisorData.find(a => a.id === student.advisorId);
-        const marker = markerData.find(m => m.id === student.markerId);
-
-        // Find an available time slot for the student and assign it
-        const availableTimeSlot = findAvailableTimeSlot(student, advisor, marker);
-        if (availableTimeSlot) {
-            schedule[student.id] = availableTimeSlot;
-            advisor.assignedStudents.push(student.id);
-            marker.assignedStudents.push(student.id);
-        } else {
-            console.log(`No available time slot for student ${student.name}`);
-        }
-
-        // If a second marker is needed, try to assign one
-        if (student.needsSecondMarker) {
-            const secondMarker = markerData.find(m => m.id !== student.markerId);
-            const availableTimeSlotForSecondMarker = findAvailableTimeSlot(student, advisor, secondMarker);
-            if (availableTimeSlotForSecondMarker) {
-                student.secondMarkerId = secondMarker.id;
-                secondMarker.assignedStudents.push(student.id);
-            } else {
-                console.log(`No available time slot for second marker for student ${student.name}`);
+        for (const student of advisorStudents) {
+            const presentationDate = findAvailablePresentationDate(advisor, presentationDates);
+            if (!presentationDate) {
+                console.log(`No available presentation date for student ${student.id}`);
+                continue;
             }
+
+            const classroomNumber = findAvailableClassroom(classroomNumbers, presentationDate);
+            if (!classroomNumber) {
+                console.log(`No available classroom for student ${student.id}`);
+                continue;
+            }
+
+            const presentation = { //the form the presentation is in 
+                date: presentationDate,
+                time: getTimeSlotForPresentation(presentationDate), // Implement this function
+                classroom: classroomNumber,
+                studentId: student.id,
+                advisorId: advisor.id,
+                markerId: student.markerId,
+                secondMarkerId: student.secondMarkerId || "No Second Marker",
+                projectTitle: student.projectTitle,
+            };
+
+            schedule.push(presentation);
         }
     }
 
     return schedule;
 }
 
-function findAvailableTimeSlot(student, advisor, marker) {
-    // TODO: finding unassigned available timeslot
-}
+// TODO: Implement the findAvailablePresentationDate and getTimeSlotForPresentation functions
 
-function scheduleHasConflict(timeSlot, student, advisor, marker) {
-    // TODO: checking schedule for conflicts
-}
-
-// Function to override/modify a timeslot
-function applyManualOverrides(schedule, studentData) {
-    for (const student of studentData) {
-        if (student.manualTimeSlot) {
-            schedule[student.id] = student.manualTimeSlot;
-        }
-    }
-}
-
-
-//to start the process
-main();
--- presentation_scheduler/Presentation_README.md
+++ presentation_scheduler/Presentation_README.md
@@ -62,3 +62,14 @@ what I need to ping back:
 4 There are 2 sessions per day am/pm 
 
 5 One of the markers is s session "chair" who runs the session. This is where it gets a bit messy and I'm a bit unclear how they want this actually implemented. Normally kate is the session chair for quite literally all of our presentation but some other conveners assign someone else. Further there was talk of trying to group people with the same supervisor/same project together in the same session if possible. I'm not certain how much of this is a stretch goal and how much is MVP but I'll leave it to your judgement.
+
+## Reconsideration and restarting the algorithm
+
+After thinking about the problem longer, I realised a few things: 
+1. We are making a schedule generator. At this stage of the program, the project allocation is done, the marker allocation is done, all we need to do is generate a presentation schedule in accordance to the preferences of the advisor. So, we need to pull relevant required info from database: all the student info, the project they’re doing, their assigned advisors and markers, relevant info about the project. 
+2. Each student has 1 advisor and 1 or 2 markers. For a scheduled presentation, the student, the advisor and the marker(s) have to all be there. We ignore the student and marker’s preferences and only tend to the preference of the advisor. 
+3. The presentation dates are fixed for ideally 2 days but maximum can be 3. If we are over capacity, more classes are run in parallel over those 2 to 3 days.
+4. The algorithm should take in these new info: Class numbers (list of classroom codes for the classes are run in parallel that day for presentation), dates that the presentation is set on (2-3 dates in the form of DD/MM/YYYY), and the advisors’ date preference.
+5. The schedule output should look something like this [“DateOfPresentation” “TimeOfPresentation” “ClassRoomNumber” “StudentID1” “AdvisorID1” “MarkerID1” “SecondMarkerID” “ProjectTitle”], [“DateOfPresentation” “TimeOfPresentation” “ClassRoomNumber” “StudentID2” “AdvisorID2” “MarkerID2” “No Second Marker” “ProjectTitle”], … With values it looks something like this: [06/11/2023,9:45am,4RPD102,46012345,40000000,40000001,40000002,Optimal arial pathing for drones]
+
+So I decided to scrap the parts of the code where I am trying to allocate markers and advisors, and rethink about what info i have and don't have at this stage.
