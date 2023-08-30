e81f9ce919e12c0c66e6d5e3407c8bf809a6acb5
D4v1dHu0 <88366550+D4v1dHu0@users.noreply.github.com>
2023-08-30 20:50:10 +1000

    added getTimeSlotForPresentation function and helper funcs
    calculates available timeslots and creates an array to track them with true = empty and false = full. the helper funcs converts between time slot index and actual time.

--- presentation_scheduler/PresentationScheduler.js
+++ presentation_scheduler/PresentationScheduler.js
@@ -15,23 +15,22 @@
 
 //TODO: NEED TO FETCH DATA SOMEHOW
 //PROBLEM: Still need a way to simulate fetch data in order to test code!!!
+
 async function fetchData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
+// something here TODO
     return data;
 }
 
 // Assuming we have fetched the required data
 
 function generatePresentationSchedule(studentData, advisorData, classroomNumbers, presentationDates) {
-    const schedule = [];
+    const schedule = []; //to populate for final schedule
 
     for (const advisor of advisorData) {
-        const advisorStudents = studentData.filter(student => student.advisorId === advisor.id);
+        const advisorOfStudents = studentData.filter(student => student.advisorId === advisor.id);
 
-        // Sort advisorStudents based on project preferences if needed
-
-        for (const student of advisorStudents) {
+        // Sort advisor of Students based on project preferences
+        for (const student of advisorOfStudents) {
             const presentationDate = findAvailablePresentationDate(advisor, presentationDates);
             if (!presentationDate) {
                 console.log(`No available presentation date for student ${student.id}`);
@@ -64,3 +63,40 @@ function generatePresentationSchedule(studentData, advisorData, classroomNumbers
 
 // TODO: Implement the findAvailablePresentationDate and getTimeSlotForPresentation functions
 
+function getTimeSlotForPresentation(presentationDate, scheduledPresentations) {
+    // Calculate the total number of time slots from 9:00 am to 3:00 pm
+    const totalSlots = (3 * 60 - 9 * 60) / 15; //should be 24 slots
+
+    // Create an array to track available time slot; true = empty, false = assigned
+    const availableSlots = new Array(totalSlots).fill(true); //initialized as true
+
+    // Mark time slots that are already scheduled as unavailable
+    for (const presentation of scheduledPresentations) {
+        if (presentation.date === presentationDate) {
+            const timeSlotIndex = getTimeSlotIndex(presentation.time);
+            availableSlots[timeSlotIndex] = false;
+        }
+    }
+
+    // Find the index of the first available time slot
+    const availableSlotIndex = availableSlots.indexOf(true);
+    
+    if (availableSlotIndex !== -1) {
+        return getTimeSlotByIndex(availableSlotIndex);
+    }
+
+    return null; // No available time slot on this date
+}
+
+function getTimeSlotIndex(time) {
+    const [hours, minutes] = time.split(":").map(Number);
+    const totalMinutes = hours * 60 + minutes;
+    return (totalMinutes - 9 * 60) / 15;
+}
+
+function getTimeSlotByIndex(index) {
+    const totalMinutes = 9 * 60 + index * 15;
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    return `${hours}:${minutes === 0 ? '00' : minutes}`;
+}
