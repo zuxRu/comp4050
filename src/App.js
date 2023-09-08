d2d9a17e0149b722d7f1d0eaff34b6f879aa6c4d
Rajiv Mehta <48074518+MystifyingEwok@users.noreply.github.com>
2023-09-08 14:55:15 +1000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,46 +1,61 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
-
-import SideNav from './navigation/SideNav';
-import View from "./pages/students/ViewProjects";
-import SubmitPref from "./pages/students/SubmitPref";
-import SubmitSelfPref from "./pages/students/SubmitSelfPref";
-import Feedback from "./pages/general/feedback";
-import TopNav from "./navigation/TopNavBar";
-import Dashboard from "./pages/general/Dashboard";
-import Login from "./pages/general/Login";
-import navDataAcademics  from "./navigation/NavBarAcademics";
-import Academics from "./pages/academics/Academics";
-import PresentationSchedule from "./pages/academics/PresentationSchedule";
-import ProjectAllocation from "./pages/academics/ProjectAllocation";
-
+import "./App.css";
+import fakeData from "./DemoAlocation.json";
+import * as React from "react";
+import { useTable } from "react-table";
 
 function App() {
-  return (
-    //<div className="App">
+  const data = React.useMemo(() => fakeData, []);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "StudentID",
+        accessor: "s_ID"
+    },
+    {
+        Header: "Project",
+        accessor: "project_ID"
+    },
+    {
+        Header: "Supervisor",
+        accessor: "supervisor_ID"
+    }
+    ],
+    []
+  );
 
-      //<div className="contents">
-        
-        <main>
-          
-          <Routes>  
-            <Route path="/" element={<Login />}/>
-            <Route element={<><TopNav className="App"/><SideNav className="contents"/></>}/>
-            <Route path="/Academics/*" element={<><TopNav/><div className="contents"><SideNav /><Dashboard/></div> </>}/>
-            <Route path="/dashboard/*" element={<><TopNav/><div className="contents"><SideNav /><Dashboard/></div> </>}/>
-            <Route path="/view-thesis-projects" element={<><TopNav/><div className="contents"><SideNav /><View/></div> </>}/>
-            <Route path="/submit-preferences" element={<><TopNav/><div className="contents"><SideNav /><SubmitPref/></div> </>} />
-            <Route path="/submit-selfproposed-preferences" element={<><TopNav/><div className="contents"><SideNav /><SubmitSelfPref/></div> </>}/>
-            <Route path="/submit-feedback" element={<><TopNav/><div className="contents"><SideNav /><Feedback/></div> </>}/>
-          </Routes>
-        </main>
-      //</div>
-    //</div>
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
+  return (
+    <div className="App">
+      <div className="container">
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
